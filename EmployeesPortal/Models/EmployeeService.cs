3c920af48a977a178e5fff59e9b360b3e4567490
Zellos <Zellos@WIN-UFO1CIKLBV0>
2024-09-12 10:19:07 +0900

    Add Employees DONE

--- EmployeesPortal/Models/EmployeeService.cs
+++ EmployeesPortal/Models/EmployeeService.cs
@@ -57,7 +57,85 @@ namespace EmployeesPortal.Models
 				Type = EmployeeType.Contract,
 				Pol = "ЖЕН",
 				Salary = 50000
-			}
-		};
+			},
+            new Employee
+            {
+                ID=5,
+                FullName = "Фёдор Сергеевич Фролов",
+                Email="Serg@frolov.com",
+                Position = "Администратор",
+                Departament = Departament.Admin,
+                HireData = DateTime.Now.AddYears(-8),
+                DateOfBirth = DateTime.Now.AddYears(-36),
+                Type = EmployeeType.Permanent,
+                Pol = "МУЖ",
+                Salary = 100000
+            },
+            new Employee
+            {
+                ID=6,
+                FullName = "Александр Александрович Легостаев",
+                Email="AA@legostaev.com",
+                Position = "Администратор",
+                Departament = Departament.Admin,
+                HireData = DateTime.Now.AddYears(-30),
+                DateOfBirth = DateTime.Now.AddYears(-65),
+                Type = EmployeeType.Permanent,
+                Pol = "МУЖ",
+                Salary = 100000
+            },
+            new Employee
+            {
+                ID=7,
+                FullName = "Никита Сергеевич Сивков",
+                Email="Nic@siv.com",
+                Position = "Программист",
+                Departament = Departament.IT,
+                HireData = DateTime.Now.AddYears(-1),
+                DateOfBirth = DateTime.Now.AddYears(-32),
+                Type = EmployeeType.Temporary,
+                Pol = "МУЖ",
+                Salary = 80000
+            },
+            new Employee
+            {
+                ID=8,
+                FullName = "Анна Александровна Линейцева",
+                Email="Anna@lin.com",
+                Position = "Менеджер",
+                Departament = Departament.HR,
+                HireData = DateTime.Now.AddYears(-10),
+                DateOfBirth = DateTime.Now.AddYears(-45),
+                Type = EmployeeType.Contract,
+                Pol = "ЖЕН",
+                Salary = 95000
+            },
+            new Employee
+            {
+                ID=9,
+                FullName = "Марина Андреевна Шорстова",
+                Email="Mar@shorst.com",
+                Position = "Менеджер",
+                Departament = Departament.HR,
+                HireData = DateTime.Now.AddYears(-20),
+                DateOfBirth = DateTime.Now.AddYears(-53),
+                Type = EmployeeType.Contract,
+                Pol = "ЖЕН",
+                Salary = 95000
+            },
+            new Employee
+            {
+                ID=10,
+                FullName = "Ольга Николаевна Горбачёва",
+                Email="Olga@Gorb.com",
+                Position = "Администратор",
+                Departament = Departament.Admin,
+                HireData = DateTime.Now.AddYears(-15),
+                DateOfBirth = DateTime.Now.AddYears(-48),
+                Type = EmployeeType.Permanent,
+                Pol = "ЖЕН",
+                Salary = 150000
+            }
+        };
 	}
 }
